996845b7c5994125f2cef5b653fe0c2a33eb51ff
Djazia Hababela <d.hababela@esi-sba.dz>
2020-05-18 23:08:32 +0100

    Jeux à 2 niveaux

---
+++ app/src/main/java/com/example/jeudesmots/Entities/Player.java
@@ -0,0 +1,34 @@
+package com.example.jeudesmots.Entities;
+
+public class Player {
+
+    private String name;
+    private int score;
+    private int level;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+}
